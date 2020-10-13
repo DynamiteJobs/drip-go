e0e862a58212fb9e6c4be109569b157628abdc1c
Simon Payne <github@kojip.com>
2020-10-13 13:07:04 +0200

    Support for names

--- client.go
+++ client.go
@@ -182,6 +182,9 @@ func (c *Client) ListSubscribers(req *ListSubscribersReq) (*SubscribersResp, err
 // UpdateSubscriber is the info available to update or create a subscriber.
 type UpdateSubscriber struct {
 	Email         string            `json:"email,omitempty"`
+	Name          string            `json:"name,omitempty"`
+	FistName      string            `json:"first_name,omitempty"`
+	LastName      string            `json:"last_name,omitempty"`
 	ID            string            `json:"id,omitempty"`
 	NewEmail      string            `json:"new_email,omitempty"`
 	UserID        string            `json:"user_id,omitempty"`
